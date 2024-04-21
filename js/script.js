3d82cf42fe6f018c80d6dcfe11a3be88fb9271ea
Garrett Moody <extrillieos@gmail.com>
2024-04-20 20:36:38 -0700

    footer for instagram

--- js/script.js
+++ js/script.js
@@ -10,17 +10,3 @@ menu.onclick = () => {
 insta.onclick = () => {
     window.open("https://www.instagram.com/edwardswatersports/?igshid=MzRlODBiNWFlZA%3D%3D");
 };
-
-
-
-const sr = ScrollReveal ({
-    distance: '65px',
-    duration: 2600,
-    delay: 450,
-    reset: true
-});
-
-sr.reveal('.hero-text', {delay:200, origin:'top'});
-sr.reveal('.hero-img', {delay:450, origin:'top'});
-sr.reveal('.icons', {delay:500, origin:'left'});
-sr.reveal('.scroll-down', {delay:200, origin:'right'});
