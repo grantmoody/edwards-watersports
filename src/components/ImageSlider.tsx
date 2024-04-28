6a34b7d6ebf223fbb0fa6ab121de4e3a8af55ce2
Garrett Moody <extrillieos@gmail.com>
2024-04-27 21:16:14 -0700

    image slider

---
+++ src/components/ImageSlider.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import { Typography } from '@mui/material';
+
+interface ImageSliderProps {
+    images: string[];
+}
+
+const ImageSlider: React.FC<ImageSliderProps> = ({ images }) => {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 1000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true, // Enable auto-scroll
+        autoplaySpeed: 10000,
+    };
+
+    return (
+        <Slider {...settings}>
+            {images.map((imageUrl, index) => (
+                <div key={index} style={{ textAlign: 'center' }}>
+                    <Typography variant='h5' mt={3}>
+                        <strong>
+                            Sea-Doo Spark Trixx {index + 2}up
+                        </strong>
+                    </Typography>
+                    <img src={imageUrl} alt={`Slide ${index + 1}`} className='trixxImage' />
+                </div>
+            ))}
+        </Slider>
+    );
+};
+
+export default ImageSlider;
--- src/page/moreInfo.tsx
+++ src/page/moreInfo.tsx
@@ -1,13 +1,15 @@
 import { Container, Typography, Grid } from "@mui/material";
 import React from "react";
+import ImageSlider from "../components/ImageSlider";
 
 const MoreInfo: React.FC = () => {
+    const images = [
+        'sparktrixx.png',
+        'sparktrixx3.png',
+    ];
 
     return (
         <Container className="moreInfo">
-            <Typography variant="h2" mt={2}>
-                <strong>More Info</strong>
-            </Typography>
             <Typography variant="h4" mt={3}>
                 Our Models
             </Typography>
@@ -20,26 +22,13 @@ const MoreInfo: React.FC = () => {
                     </Typography>
                 </Grid>
             </Grid>
-            <Grid container justifyContent="center" spacing={5} mt={1}>
-                <Grid item xs={12} sm={6} md={6} lg={6}>
-                    <Typography variant="subtitle1" align="center" component="div">
-                        <strong>Sea-Doo Spark Trixx 2ups</strong>
-                    </Typography>
-                    <img src="/sparktrixx.png" alt="Sea-Doo Spark Trixx 2ups" className="trixxImage" />
-                </Grid>
-                <Grid item xs={12} sm={6} md={6} lg={6}>
-                    <Typography variant="subtitle1" align="center" component="div">
-                        <strong>Sea-Doo Spark Trixx 3up</strong>
-                    </Typography>
-                    <img src="/sparktrixx3.png" alt="Sea-Doo Spark Trixx 3up" className="trixxImage" />
-                </Grid>
-            </Grid>
+            <ImageSlider images={images}></ImageSlider>
             <Typography variant="h4" mt={4}>
                 Rental Information
             </Typography>
             <Grid container justifyContent="center" >
                 <Grid item>
-                    <Typography mt={3} maxWidth={"500px"}>
+                    <Typography mt={3} mb={5} maxWidth={"500px"}>
                         We offer our services to Hayden and Coeur d' Alene Lake. Before you are allowed to ride the jetskis, we kindly ask that you review our waiver to fill out in person, and also watch the informational videos provided on our website.
                     </Typography>
                 </Grid>
