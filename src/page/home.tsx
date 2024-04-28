3c848fca521fb8d01186dce51d666b6692122c35
MatijaBenko <matijabenko@yahoo.com>
2024-04-27 18:44:12 -0700

    Implemented home page with navbar template

---
+++ src/page/home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from "../components/navbar";
+
+const Home: React.FC = () => {
+    return(
+        <>
+            <NavBar/>
+        </>
+    )
+}
+
+export default Home;
