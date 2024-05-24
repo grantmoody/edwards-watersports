a8026322eea859cf00c382f56553b86c9ff8c7fa
Garrett Moody <extrillieos@gmail.com>
2024-05-23 23:27:49 -0700

    reponsive logo size

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -37,7 +37,7 @@ function ResponsiveAppBar() {
                 <Toolbar disableGutters>
                     <Box sx={{ display: 'flex', alignItems: 'center', flexGrow: 1, justifyContent: 'space-between' }}>
                         <span style={{ display: 'inline-block', verticalAlign: 'middle' }}>
-                            <img src="edwards_watersports_logo.png" style={{ width: '300px', paddingTop: '20px', paddingLeft: '2vw', verticalAlign: 'middle' }} />
+                            <img src="edwards_watersports_logo.png" className='jetskilogo' />
                             {/* <Typography
                                 variant="h5"
                                 noWrap
